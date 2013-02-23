a4590394818679211fc8a3f0d071b8367e598669
Max Shytikov <mshytikov@gmail.com>
2013-02-23 11:24:20 +0200

    first refactoring done

--- lib/problem1.rb
+++ lib/problem1.rb
@@ -1,7 +1,7 @@
-
 module Problem1
 
   NUMBER_TO_WORD= {
+    0 => "",
     1 => "one",
     2 => "two",
     3 => "three",
@@ -38,32 +38,38 @@ module Problem1
 
   AND_COUNT= 3
 
-  def self.letters_count(number)
-    raise(ArgumentError, "Supported numbers < 10000")  if number > 9999
-    return 0 if number.zero?
+  class << self
 
-    letters_count = NUMBER_TO_LETTERS_COUNT[number]
-    return letters_count if letters_count
+    def letters_count(number)
 
-    if number >= 1000 and number < 9999
-      thousands_count = (number / 1000)
-      prefix_len = letters_count(thousands_count) + THOUSAND_COUNT
-      if (number % 1000).zero?
-        prefix_len
-      else
-        prefix_len  + AND_COUNT + letters_count(number - thousands_count*1000)
+      case 
+      when number > 1000
+        raise ArgumentError, "Number must be <= 1000"
+
+      when predefined_count(number)
+        predefined_count(number)
+
+      when number == 1000
+        thousands_count = (number / 1000)
+        letters_count(thousands_count) + THOUSAND_COUNT
+
+      when number >= 100 
+        hundreds_count = (number / 100)
+        result  = letters_count(hundreds_count) + HUNDRED_COUNT
+
+        unless (number % 100).zero? 
+          result += AND_COUNT + letters_count(number - hundreds_count*100)
+        end
+        result
+
+      when number > 20
+        tens_count = (number / 10)
+        predefined_count(tens_count*10) + letters_count(number - tens_count*10)
       end
-    elsif number >= 100 
-      hundreds_count = (number / 100)
-      prefix_len = letters_count(hundreds_count) + HUNDRED_COUNT 
-      if (number % 100).zero?
-        prefix_len
-      else
-        prefix_len + AND_COUNT + letters_count(number - hundreds_count*100)
-      end
-    elsif number > 20
-      tens_count = (number / 10)
-      NUMBER_TO_LETTERS_COUNT[tens_count*10] + letters_count(number - tens_count*10)
+    end
+
+    def predefined_count(number)
+      NUMBER_TO_LETTERS_COUNT[number]
     end
   end
 
