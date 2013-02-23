944dc4f38b6e6751c6705edde54b756bb52fa4fb
Max Shytikov <mshytikov@gmail.com>
2013-02-23 10:14:29 +0200

    added one more test

--- spec/problem1_spec.rb
+++ spec/problem1_spec.rb
@@ -7,6 +7,7 @@ describe Problem1 do
     it { Problem1.letters_count(15).should == 7 }
     it { Problem1.letters_count(342).should == 23 }
     it { Problem1.letters_count(115).should == 20 }
+    it { Problem1.letters_count(42).should == 8 }
   end
 
 
