d36222c571d333d8654da48f002daa4a2ce47341
Max Shytikov <mshytikov@gmail.com>
2013-02-23 10:33:05 +0200

    pass all tests

--- lib/problem1.rb
+++ lib/problem1.rb
@@ -28,13 +28,13 @@ module Problem1
     60 => "sixty",
     70 => "seventy",
     80 => "eighty",
-    90 => "ninety",
-    100 => "hundred",
-    1000 => "thousand",
+    90 => "ninety"
   }
 
   NUMBER_TO_LETTERS_COUNT  = Hash[NUMBER_TO_WORD.map{|k,v| [k, v.size]}]
 
+  HUNDRED_COUNT = "hundred".size
+  THOUSAND_COUNT = "thousand".size
 
   AND_COUNT= 3
 
@@ -45,12 +45,22 @@ module Problem1
     letters_count = NUMBER_TO_LETTERS_COUNT[number]
     return letters_count if letters_count
 
-    if number > 1000 and number < 9999
-      thousands_count = (number / 1000)
-      letters_count(thousands_count) + NUMBER_TO_LETTERS_COUNT[1000]  + AND_COUNT + letters_count(number - thousands_count*1000)
-    elsif number > 100 
+    if number >= 1000 and number < 9999
+        thousands_count = (number / 1000)
+        prefix_len = letters_count(thousands_count) + THOUSAND_COUNT
+        if (number % 1000).zero?
+          prefix_len
+        else
+          prefix_len  + AND_COUNT + letters_count(number - thousands_count*1000)
+        end
+    elsif number >= 100 
       hundreds_count = (number / 100)
-      letters_count(hundreds_count) + NUMBER_TO_LETTERS_COUNT[100]  + AND_COUNT + letters_count(number - hundreds_count*100)
+      prefix_len = letters_count(hundreds_count) + HUNDRED_COUNT 
+      if (number % 100).zero?
+        prefix_len
+      else
+        prefix_len + AND_COUNT + letters_count(number - hundreds_count*100)
+      end
     elsif number > 20
       tens_count = (number / 10)
       NUMBER_TO_LETTERS_COUNT[tens_count*10] + letters_count(number - tens_count*10)
