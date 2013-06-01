aedc275daed2075c2b2886e490cd6086bb12aa77
Max Shytikov <mshytikov@gmail.com>
2013-06-01 09:39:08 +0300

    added test for user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
+
   has_secure_password
+
+  validates_uniqueness_of :email, :case_sensitive => false
   validates_presence_of :password, :on => :create
+  validates_presence_of :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,22 +2,30 @@ require 'spec_helper'
 
 describe User do
   describe ".create" do
+    let(:email) { FactoryGirl.generate(:email) }
+    let(:password) { FactoryGirl.generate(:password) }
+
     subject { User.create(email: email, password: password) }
 
+    context "email and password are valid" do
+      it { should have(:no).errors }
+    end
+
     context "email is blank" do
-      let(:email) { nil }
+      let(:email) { "" }
       it { should have(1).error_on(:email) }
     end
 
     context "password is blank" do
-      let(:email) { nil }
+      let(:password) { "" }
       it { should have(1).error_on(:password) }
     end
 
     context "email already exists" do
       before { FactoryGirl.create(:user, email: email) }
-      it { should have(1).error_on(:base) }
+      it { should have(1).error_on(:email) }
     end
+
 
   end
 end
