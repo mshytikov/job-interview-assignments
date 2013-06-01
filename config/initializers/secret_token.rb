40188b1a828865b47b8ab91ef7646849041a72e5
Max Shytikov <mshytikov@gmail.com>
2013-06-01 08:25:27 +0300

    initial commit. init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleBankingApp::Application.config.secret_token = '6c2a895ec862856a38fa10a1c96477b322e6ee6f419815c307128dfb99c33559283d9958d8289fede6c780675e4a43e0e83959b515c40ae5b2eebf152baae9e3'
