2a0efb1d609f8bc551a58116a7f901681cac52a7
Max Shytikov <mshytikov@gmail.com>
2013-09-22 01:30:48 +0300

    added tests for convertes controller

--- spec/controllers/converters_controller_spec.rb
+++ spec/controllers/converters_controller_spec.rb
@@ -10,10 +10,67 @@ describe ConvertersController do
   end
 
   describe "GET 'convert'" do
-    it "returns http success" do
-      get 'convert'
-      response.should be_success
+    shared_examples_for "successful convert request" do
+      subject { response }
+      it { should be_success }
+      its("body") { should == expected_body.to_json }
+    end
+
+    shared_examples_for "bad convert request" do
+      subject { response }
+      it { should_not be_success }
+      its("status") { should == 400 }
+    end
+
+    describe "succesfull cases" do
+      context "with one value" do
+        let(:expected_body) {
+          {
+            from:   :celsium,
+            to:     :fahrenheit,
+            values: { 5 => 41 }
+          }
+        }
+        before { get 'convert', { from: 'celsium', to: 'fahrenheit', values: "5" } }
+        it_behaves_like "successful convert request"
+      end
+
+      context "with multiple value" do
+        let(:expected_body) {
+          {
+            from:   :celsium,
+            to:     :fahrenheit,
+            values: { 4 =>  41, 0 => 32 }
+          }
+        }
+        before { get 'convert', { from: 'celsium', to: 'fahrenheit', values: "5, 0" } }
+        it_behaves_like "successful convert request"
+
+      end
+    end
+
+    describe "unsuccessful cases" do
+      context "with invalid 'values' parameter" do
+          before { get 'convert', { from: 'celsium', to: 'fahrenheit', values: "abc" } }
+          it_behaves_like "bad convert request"
+      end
+
+      context "without required  params" do
+        context "without 'from' " do
+          before { get 'convert', { to: 'celsium', values: "1" } }
+          it_behaves_like "bad convert request"
+        end
+        context "without 'to' " do
+          before { get 'convert', { to: 'celsium', values: "1" } }
+          it_behaves_like "bad convert request"
+        end
+
+        context "without 'values' " do
+          before { get 'convert', {form: 'fahrenheit', to: 'celsium'} }
+          it_behaves_like "bad convert request"
+        end
+      end
+
     end
   end
-
 end
