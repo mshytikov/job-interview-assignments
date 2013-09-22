2baea7cc27ede43a3b6900ffcf89156393d088a2
Max Shytikov <mshytikov@gmail.com>
2013-09-22 16:16:19 +0300

    refactor impl of converter_controller

--- app/controllers/converters_controller.rb
+++ app/controllers/converters_controller.rb
@@ -3,11 +3,11 @@ class ConvertersController < ApplicationController
   end
 
   def convert
-    @result =  { 
-      :from => params.fetch(:from),
-      :to   => params.fetch(:to),
-      :values => Hash[convert_values.map{|v| [v, v] }]
-    }
+    from = params.fetch(:from)
+    to = params.fetch(:to)
+    values = Hash[params_fetch_values.map{|v| [v, Converter.convert(from, to,v)] }]
+
+    @result =  { from: from, to: to, values: values}
 
     respond_to do |format|
       format.html
@@ -17,9 +17,10 @@ class ConvertersController < ApplicationController
 
   private
 
-  def convert_values
+  def params_fetch_values
     values = params.fetch(:values)
-    values = values.split(/,\s*/) unless values.is_a?(Array)
+    #support of different format for 'values' param: &values=1,2 or &values[]=1&values[]=2
+    values = values.split(/,\s*/) unless values.is_a?(Array) 
     values.map{|v| Float(v) }
   end
 
