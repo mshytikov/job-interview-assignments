93e6b4aa52e69c6511fdd6402942f59483b169fd
Max Shytikov <mshytikov@gmail.com>
2013-09-22 15:27:11 +0300

    added conversion tests

--- spec/models/converter_spec.rb
+++ spec/models/converter_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Converter do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe ".celcius_to_fahrenheit(value)" do
+    subject{ Converter.celcius_to_fahrenheit(value) }
+    context "with integer value" do
+      let(:value) { 1 }
+      its { should == 33.8}
+    end
+    context "with float value" do
+      let(:value) { 1.0 }
+      its { should == 33.8}
+    end
+  end
+
+  describe ".fahrenheit_to_celcius" do
+    subject{ Converter.fahrenheit_to_celcius(value) }
+    context "with integer value" do
+      let(:value) { 1 }
+      its { should == -17.2222}
+    end
+    context "with float value" do
+      let(:value) { 1.0 }
+      its { should == -17.2222}
+    end
+  end
+
+  describe ".convert" do
+
+  end
+
+
 end
