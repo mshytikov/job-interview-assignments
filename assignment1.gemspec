2daabb25ca9ce6d6659d05b52d69d59b225854fa
Max Shytikov <mshytikov@gmail.com>
2013-09-23 18:11:53 +0300

    added vcr gem

--- assignment1.gemspec
+++ assignment1.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec", "~> 2.14.1"
+  spec.add_development_dependency "vcr",   "~> 2.5.0"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,14 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 #
 require 'assignment1'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
