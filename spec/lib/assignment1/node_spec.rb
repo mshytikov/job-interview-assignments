e3be68ec72437cde9f5d2ecd2cda0f866d87de22
Max Shytikov <mshytikov@gmail.com>
2013-09-23 15:53:09 +0300

    added tests for Node#add_child mehthod

--- spec/lib/assignment1/node_spec.rb
+++ spec/lib/assignment1/node_spec.rb
@@ -2,28 +2,30 @@ require 'spec_helper'
 
 
 describe Assignment1::Node do
+  def uri(path)
+    "http://assignment1.droxbob.com/#{path}"
+  end
+  let(:node) { Assignment1::Node.new(uri(nil)) }
+
 
   describe ".new" do
-    let(:url) { "http://test.droxbob.com/a.html" }
-    subject { Assignment1::Node.new(url) }
+    subject { node }
     it { should respond_to(:id) }
-    its(:id) { should == url }
+    its(:id) { should == "http://assignment1.droxbob.com/" }
+    its(:children) { should == [] }
+    its(:children_inputs_count) { should == 0 }
 
     it { should respond_to(:build) }
     it { should respond_to(:links) }
-    it { should respond_to(:childrens) }
+    it { should respond_to(:children) }
     it { should respond_to(:add_child) }
     it { should respond_to(:inputs_count) }
-    it { should respond_to(:total_inputs_count) }
+    it { should respond_to(:children_inputs_count) }
     it { should respond_to(:to_s) }
   end
 
   describe "#build" do
-    def uri(path)
-      "http://assignment1.droxbob.com/#{path}"
-    end
 
-    let(:node) { Assignment1::Node.new(uri(nil)) }
 
     it "respond with true" do
       expect{ node.build }.to be_true
@@ -37,5 +39,18 @@ describe Assignment1::Node do
     it "fills input_counts" do
       expect{ node.build }.to change{ node.inputs_count }.from(nil).to(2)
     end
+  end
+
+  describe "#add_child" do
+    let(:child) { Assignment1::Node.new(uri("a.html")) }
+    before { child.build }
+    it "increases total_inputs_count"  do
+      expect{ node.add_child(child) }.to change{ node.children_inputs_count }.from(0).to(2)
+    end
+
+    it "adds child" do
+      expect{ node.add_child(child) }.to change{ node.children }.from([]).to([child])
+    end
+
   end
 end
