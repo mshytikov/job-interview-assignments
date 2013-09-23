9eb82c65ec412e09138bc906f831781eafd116bd
Max Shytikov <mshytikov@gmail.com>
2013-09-24 00:03:44 +0300

    added tests for module Visitable

---
+++ spec/lib/assignment1/visitable_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+
+describe Assignment1::Visitable do
+  class Dummy
+    include Assignment1::Visitable
+  end
+
+  describe "include Visitable" do
+    subject(:obj) { Dummy.new }
+    it{ should respond_to?(:visit) }
+    it{ should respond_to?(:visited?) }
+    its(:visited?) { should be_false}
+
+    describe "#.visit!(&block)" do
+      it "call block with self" do
+        target = nil
+        expect{ obj.visit!{|e| target = e } }.to change{ target }.from(nil).to(obj);
+      end
+      it "change #visited? to true" do
+        expect{ obj.visit!{} }.to change{ obj.visited? }.from(false).to(true);
+      end
+
+    end
+  end
+end
