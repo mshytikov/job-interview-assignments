2c868732578715d239849757f04c784176f85f2e
Max Shytikov <mshytikov@gmail.com>
2013-09-23 20:38:48 +0300

    added impl for #build_tree

--- lib/assignment1.rb
+++ lib/assignment1.rb
@@ -6,4 +6,25 @@ require "assignment1/node"
 
 module Assignment1
 
+  class << self
+
+    def build_tree(url, deep = 3, limit = 50)
+      visit_node(url, 0, 0, deep, limit)
+    end
+
+    def visit_node(url, level, nodes_count, deep, limit)
+      node = Node.new(url).build
+      nodes_count += 1
+      if level < deep
+        node.links.each{|link|
+            break if nodes_count >= limit
+            child = visit_node(link, level + 1, nodes_count, deep, limit)
+            node.add_child(child)
+            nodes_count += 1
+        }
+      end
+      node
+    end
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ VCR.configure do |c|
 end
 
 RSpec.configure do |config|
+  config.extend VCR::RSpec::Macros
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
