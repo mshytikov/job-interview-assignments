8243dd64ef7d97ff4388c88a3c4fd812e5208041
Max Shytikov <mshytikov@gmail.com>
2014-03-22 22:39:00 +0100

    added select menu for compaign banners

--- assignment7/ad_min/app/helpers/campaign_banners_helper.rb
+++ assignment7/ad_min/app/helpers/campaign_banners_helper.rb
@@ -1,5 +1,12 @@
 module CampaignBannersHelper
 
+  # Returns available banneres for select tag
+  def available_banners(campaign)
+    campaign.available_banners.map do |b|
+      [b.name, b.id, {'data-image' => b.image.url}]
+    end
+  end
+
   # Workaround for `form_for` with nested resources.
   # I have described it here https://github.com/rails/rails/issues/14451
 
--- assignment7/ad_min/app/models/campaign.rb
+++ assignment7/ad_min/app/models/campaign.rb
@@ -1,3 +1,11 @@
 class Campaign < ActiveRecord::Base
   has_many :campaign_banners, dependent: :destroy
+
+  def available_banners
+    if campaign_banner_ids.empty?
+      Banner.all
+    else
+      Banner.where('id NOT in (?)', campaign_banner_ids)
+    end
+  end
 end
