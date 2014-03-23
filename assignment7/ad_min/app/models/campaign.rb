c69a472b313e68211a70a758d4d61d4c43d7ef7d
Max Shytikov <mshytikov@gmail.com>
2014-03-23 02:10:44 +0100

    added saving propogations to AdServe

--- assignment7/ad_min/app/models/campaign.rb
+++ assignment7/ad_min/app/models/campaign.rb
@@ -1,6 +1,9 @@
 class Campaign < ActiveRecord::Base
   has_many :campaign_banners, dependent: :destroy
 
+  after_save    {|r| AdServe.save_campaign(r.id, r.ratio_random, b.ratio_weighted) }
+  after_destroy {|r| AdServe.delete_campaign(r.id) }
+
   def available_banners
     if campaign_banner_ids.empty?
       Banner.all
--- assignment7/ad_min/app/models/campaign_banner.rb
+++ assignment7/ad_min/app/models/campaign_banner.rb
@@ -6,4 +6,7 @@ class CampaignBanner < ActiveRecord::Base
   validates_uniqueness_of :banner_id, scope: :campaign_id
 
   delegate :image, :name, to: :banner
+
+  after_save    {|r| AdServe.save_banner(r.id, r.campaign_id, r.weight, r.image_url) }
+  after_destroy {|r| AdServe.delete_banner(r.id) }
 end
