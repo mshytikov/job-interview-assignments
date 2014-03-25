79fcb55b5505f2d6bff856d6c911f78eaeaa1511
Max Shytikov <mshytikov@gmail.com>
2014-03-25 20:36:22 +0100

    Comments cleanup

--- assignment7/ad_serve/app/calculation.rb
+++ assignment7/ad_serve/app/calculation.rb
@@ -2,8 +2,11 @@ require 'securerandom'
 
 module Calculation
 
+
+  #
   # This function accepts weighted hash like {'key1' => weight1, 'key2' => weitght2,...}
   # It picks the key with weighted probability
+  #
   def self.pick_random_weighted_key(weights)
     total_weight = weights.values.inject(:+)
     random_weight = SecureRandom.random_number(total_weight)
@@ -18,8 +21,10 @@ module Calculation
     weights.keys().sample()
   end
 
+  #
   # Accepts hash like { index1 => v1, index2 => v2, ...}, and bitmask 
   # Returns hash without keys with corresponding bit '1' in bitmask
+  #
   def self.filter_indexed_hash(indexed_hash, bitmask)
     bitmask.unpack('B*').map
     indexed_hash.select{|idx, w| idx >= bitmask.size ||  bitmask[idx] == '0'}
