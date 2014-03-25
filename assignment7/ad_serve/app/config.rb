ca9a6407971fda289eb9a76c5a53bc2b2a31849b
Max Shytikov <mshytikov@gmail.com>
2014-03-25 20:10:59 +0100

    Added test config

--- assignment7/ad_serve/app/config.rb
+++ assignment7/ad_serve/app/config.rb
@@ -1,4 +1,8 @@
 configuration do |app|
-    Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
-    set :default_banner_url, 'http://google.com'
+  Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
+  set :default_banner_url, 'http://google.com'
+end
+
+configuration :test do |app|
+  Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379, :db => 1)
 end
