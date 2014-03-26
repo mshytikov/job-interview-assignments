9d82a01384d0347e2cdc2159e2006279041b7185
Max Shytikov <mshytikov@gmail.com>
2014-03-26 03:18:21 +0100

    Added test Calculation#pick_random_weighted_key

--- assignment7/ad_serve/spec/app/calculation_spec.rb
+++ assignment7/ad_serve/spec/app/calculation_spec.rb
@@ -3,13 +3,35 @@ require 'spec_helper'
 describe Calculation do 
   include Calculation
 
+  let(:input) { Hash[(0..99).map{|i| ["key_#{i}", i] }] }
+
   describe "#pick_random_key" do
-    let(:input) { Hash[(1..50).map{|i| [i.to_s, i] }] }
 
     it "picks mostly all elements" do
-      picked_keys = 1000.times.map{ pick_random_key(input) }
+      picked_keys = 10000.times.map{ pick_random_key(input) }
       (input.keys - picked_keys).size.should <= 2
     end
+
+  end
+
+  describe "#pick_random_weighted_key" do
+
+    it "picks mostly all elements" do
+      picked_keys = 1000.times.map{ pick_random_weighted_key(input) }
+      (input.keys - picked_keys).size.should <= 5
+    end
+
+    it "picks more weighted key first with high porbability" do
+      input[99] = 1000
+      keys_frequency = Hash.new(0)
+      1000.times{|i|
+        k = pick_random_weighted_key(input)
+        keys_frequency[k] += 1
+      }
+      first_key = keys_frequency.max_by{|k,v| v}.first
+      first_key.should == 99
+    end
+
   end
 end
 
