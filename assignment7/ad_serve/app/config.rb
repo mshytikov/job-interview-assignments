a1ca5be010f78528a9182cdd19a897b8af67bf76
Max Shytikov <mshytikov@gmail.com>
2014-03-26 22:53:05 +0100

    Chnaged default url when no banners to show

--- assignment7/ad_serve/app/config.rb
+++ assignment7/ad_serve/app/config.rb
@@ -1,6 +1,6 @@
 configure do |app|
   Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
-  set :default_banner_url, 'http://google.com'
+  set :default_banner_url, 'http://www.milliondollarhomepage.com/'
 end
 
 configure :test do |app|
