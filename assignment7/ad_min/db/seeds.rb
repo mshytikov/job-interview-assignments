ed4c52a165788c56d48cf49275c6eaaab92c0b67
Max Shytikov <mshytikov@gmail.com>
2014-03-26 22:48:50 +0100

    added seed data

--- assignment7/ad_min/db/seeds.rb
+++ assignment7/ad_min/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+campaign = Campaign.create({ratio_random: 60, ratio_weighted: 40})
+
+Dir[Rails.root.join('tests', 'fixtures', 'banners', '*.png')].each do |f|
+  puts f
+  Banner.create(name: File.basename(f), image: File.open(f))
+end
+
+Banner.all.each.with_index do |banner, i|
+  CampaignBanner.create(campaign: campaign, banner: banner, weight: i)
+end
+
+puts "="*50
+puts "Generated data for Campaign id: #{campaign.id}"
