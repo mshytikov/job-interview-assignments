316063cd4d5ff015c53ca99d9a3a538802ce7444
Max Shytikov <mshytikov@gmail.com>
2014-03-30 05:29:44 +0200

    Added page for tesitng banners

--- assignment7/ad_serve/app/config.rb
+++ assignment7/ad_serve/app/config.rb
@@ -1,6 +1,6 @@
 configure do |app|
   Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
-  set :default_banner_url, 'http://www.milliondollarhomepage.com/'
+  set :default_banner_url, 'http://ad-min.herokuapp.com/no_banners.jpeg'
 end
 
 configure :test do |app|
