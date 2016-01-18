0f23175d58b17170ad77861e93557e144ec4e2f7
Max Shytikov <mshytikov@gmail.com>
2016-01-19 00:10:32 +0100

    Added Plateau.parse method

--- spec/mer/plateau_spec.rb
+++ spec/mer/plateau_spec.rb
@@ -1,6 +1,31 @@
 require 'spec_helper'
 
 describe Mer::Plateau do
+  describe ".new" do
+    let(:x_max) { 10 }
+    let(:y_max) { 20 }
+    subject { described_class.new(x_max, y_max) }
+
+    its(:x_side) { is_expected.to eq(0..10) }
+    its(:y_side) { is_expected.to eq(0..20) }
+  end
+
+  describe ".parse" do
+    subject { described_class.parse(size_str) }
+
+    context "with correct size" do
+      let(:size_str) { "10 20" }
+      it { is_expected.to be_a(Mer::Plateau) }
+      its(:x_side) { is_expected.to eq(0..10) }
+      its(:y_side) { is_expected.to eq(0..20) }
+    end
+
+    context "with invalid size" do
+      let(:size_str) { "-1 20" }
+      it { expect { subject }.to raise_error(ArgumentError, /plateau size/) }
+    end
+  end
+
   describe "#include?" do
     subject { described_class.new(10, 20).include?(x, y) }
 
